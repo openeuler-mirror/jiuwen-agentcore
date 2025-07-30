8a0c8006cfcc024781e272a028c87ce52d898161
Cheng Shuo <chengshuo5@huawei.com>
2025-07-30 20:44:27 +0800

    代码更新

--- jiuwen/core/context/agent_context.py
+++
@@ -1,7 +0,0 @@
-from jiuwen.core.agent.task.task_context import TaskContext
-from jiuwen.core.context.store import Store
-
-
-class AgentContext:
-    context_map: dict[str, TaskContext] = {}
-    store: Store = None
